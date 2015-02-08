6a9a9f3be138222b43f44635a322c1e9b45c63c4
Rodrigo Valente <rodrigoavrav@gmail.com>
2015-02-08 21:13:53 -0200

    Adicionado fatoração QR.

--- LinAlg/linalg.h
+++ LinAlg/linalg.h
@@ -5,7 +5,10 @@
 
 namespace LinAlg {
     template<typename Type>
-    Type Trace(const LinAlg::Matrix<Type>& mat);
+    Type Trace (const LinAlg::Matrix<Type>& mat);
+
+    template<typename Type>
+    void QR_Factoration (const LinAlg::Matrix<Type>& input_matrix, LinAlg::Matrix<Type>& output_Q_matrix, LinAlg::Matrix<Type>& output_R_matrix);
 }
 
 #include "src/linalg.hpp"
